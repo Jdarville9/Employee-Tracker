db94a798be042258810be4d69f7715d50b7154a0
Joseph Darville <joseph.darville9@gmail.com>
2022-07-11 00:20:00 -0400

    initial commit, base file structure created

---
+++ db/seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO department (department_name)
+VALUES
+  ('Sales'),
+  ('Engineering'),
+  ('Finance'),
+  ('Legal');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+  ('Salesperson', 10000, 1),
+  ('Software Engineer', 120000, 2),
+  ('Account Manager', 150000, 3),
+  ('Accountant', 100000, 3),
+  ('Legal Team Lead', 250000, 4),
+  ('Lawyer', 175000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Finn', 'Murton', 4, 1);
